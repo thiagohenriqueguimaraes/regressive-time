6ea7d0a20087b3643f85a79516e9a6492c203e96
thiago.guimaraes <thiagohenriqueguimaraes@gmail.com>
2023-08-08 16:39:02 -0300

    create countdown

--- app/js/index.js
+++ app/js/index.js
@@ -13,3 +13,32 @@
 // import { default as index_css }  from './css/index.css'
 
 console?.log('💀')
+
+
+const countdown   = document.querySelector('countdown')
+const hour             = countdown.querySelector('#countdown-hrs')
+const minute           = countdown.querySelector('#countdown-min')
+const second           = countdown.querySelector('#countdown-seg')
+
+var date = new Date();
+date.setHours(0, 0, 0)
+var countDownDate = date.setDate(date.getDate() + 1);
+
+
+// Update the count down every 1 second
+var countDownInterval = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+    
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  hour.textContent = hours
+  minute.innerText = minutes
+  second.innerText = seconds
+
+  if (distance < 0) clearInterval(countDownInterval)
+    
+}, 1000);
